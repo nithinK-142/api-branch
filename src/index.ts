49ffb174b6fb64c5c5271092ff2a3f21fc069418
nithinK-142 <nithin.sagar.359@gmail.com>
2024-02-18 14:11:45 +0530

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { json, Request, Response } from "express";
+import { handleFaviconRequest } from "./middleware/handleFaviconRequest";
+import axios from "axios";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(json());
+
+app.use(handleFaviconRequest);
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).send("api-branch");
+});
+
+app.get("/branches/:owner?/:repo?", async (req: Request, res: Response) => {
+  const { owner, repo } = req.params;
+
+  const selectedOwner = owner || "nithinK-142";
+  const selectedRepo = repo || "nithin.me";
+  try {
+    const response = await axios.get(
+      `https://api.github.com/repos/${selectedOwner}/${selectedRepo}/branches`
+    );
+    const branches = response.data.map(
+      (branch: { name: string }) => branch.name
+    );
+    res.json(branches);
+  } catch (error) {
+    console.error("Error fetching branches:", error);
+    res.status(500).json({ error: "Check username or branchname..." });
+  }
+});
+
+app.listen(PORT, () => console.log("SERVER STARTED"));
+
+export default app;
