ee27d989af67cafd507e03bc73f8107137d923b4
nithinK-142 <nithin.sagar.359@gmail.com>
2024-02-19 13:18:02 +0530

    add: gihub access token.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { json, Request, Response } from "express";
 import cors from "cors";
+import { config } from "dotenv";
 import { handleFaviconRequest } from "./middleware/handleFaviconRequest";
 import axios from "axios";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+config();
 
 app.use(
   cors({
@@ -24,15 +27,21 @@ app.get("/:repo?/:owner?", async (req: Request, res: Response) => {
   const selectedRepo = repo || "nithin.me";
   try {
     const response = await axios.get(
-      `https://api.github.com/repos/${selectedOwner}/${selectedRepo}/branches`
+      `https://api.github.com/repos/${selectedOwner}/${selectedRepo}/branches`,
+      {
+        headers: {
+          Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        },
+      }
     );
+
     const branches = response.data.map(
       (branch: { name: string }) => branch.name
     );
-    res.json(branches);
+    return res.json(branches);
   } catch (error) {
     console.error("Error fetching branches:", error);
-    res.status(500).json({ error: "Check username or branchname..." });
+    return res.status(500).json({ error: "Check username or branchname..." });
   }
 });
 
