6dac9bc79fa488eca1e294e6ca1ff71924fb64db
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-29 19:37:21 +0530

    refactor: updated file structure.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import express, { json, Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import { config } from "dotenv";
+import "dotenv/config";
 import { handleFaviconRequest } from "./middleware/handleFaviconRequest";
-import axios from "axios";
+import { BranchRouter } from "./routes";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-config();
+const port = process.env.PORT || 3001;
 
 app.use(
   cors({
@@ -16,47 +14,11 @@ app.use(
   })
 );
 
-app.use(json());
-
+app.use(express.json());
 app.use(handleFaviconRequest);
 
-app.get("/:repo?/:owner?", async (req: Request, res: Response) => {
-  const { repo, owner } = req.params;
-  const { links } = req.query;
+app.use(BranchRouter);
 
-  const selectedOwner = owner || "nithinK-142";
-  const selectedRepo = repo || "nithin.me";
-  try {
-    const response = await axios.get(
-      `https://api.github.com/repos/${selectedOwner}/${selectedRepo}/branches`,
-      {
-        headers: {
-          Authorization: `token ${process.env.GITHUB_TOKEN}`,
-        },
-      }
-    );
-    const branches = response.data.map(
-      (branch: { name: string }) => branch.name
-    );
-    if (links) {
-      const branchesWithLinks = branches.map((branch: string) => ({
-        branch: branch,
-        link: `https://vercel.live/link/${selectedRepo.replace(
-          /\./g,
-          "-"
-        )}-git-${branch.replace(/\//g, "-")}-nithink-142.vercel.app`,
-      }));
-
-      return res.json(branchesWithLinks);
-    }
-
-    return res.json(branches);
-  } catch (error: any) {
-    console.error(error.message);
-    return res.status(500).json({ error: "Check username or branchname..." });
-  }
-});
-
-app.listen(PORT, () => console.log("SERVER STARTED"));
+app.listen(port, () => console.log("SERVER STARTED"));
 
 export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,43 @@
+import { Router, Request, Response } from "express";
+import axios from "axios";
+
+const router = Router();
+
+router.get("/:repo?/:owner?", async (req: Request, res: Response) => {
+  const { repo, owner } = req.params;
+  const { links } = req.query;
+
+  const selectedOwner = owner || "nithinK-142";
+  const selectedRepo = repo || "nithin.me";
+  try {
+    const response = await axios.get(
+      `https://api.github.com/repos/${selectedOwner}/${selectedRepo}/branches`,
+      {
+        headers: {
+          Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        },
+      }
+    );
+    const branches = response.data.map(
+      (branch: { name: string }) => branch.name
+    );
+    if (links) {
+      const branchesWithLinks = branches.map((branch: string) => ({
+        branch: branch,
+        link: `https://vercel.live/link/${selectedRepo.replace(
+          /\./g,
+          "-"
+        )}-git-${branch.replace(/\//g, "-")}-nithink-142.vercel.app`,
+      }));
+
+      return res.json(branchesWithLinks);
+    }
+
+    return res.json(branches);
+  } catch (error: any) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Check username or branchname..." });
+  }
+});
+
+export { router as BranchRouter };
