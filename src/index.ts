1c95da4b0ea528860e99f3cd97bab455bfc96429
nithinK-142 <nithin.sagar.359@gmail.com>
2024-07-04 10:51:50 +0530

    feat: disable access to unknown origins.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { corsOptions } from "./utils/utils";
 import { handleFaviconRequest } from "./middleware/favicon.middleware";
 import { BranchRouter } from "./routes";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({ origin: "*", methods: ["GET"] }));
-
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(handleFaviconRequest);
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,4 @@
+import { CorsOptions } from "cors";
 import { Repo } from "./types";
 
 export const getProjectId = (repo: string) => {
@@ -11,4 +12,24 @@ export const getProjectId = (repo: string) => {
   };
 
   return projectMap[repo] || null;
+};
+
+// cors options
+const allowedOrigins: string[] = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",").map((origin) => origin.trim())
+  : [];
+
+export const corsOptions: CorsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (error: Error | null, allow?: boolean) => void
+  ) => {
+    if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
