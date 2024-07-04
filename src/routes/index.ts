fdc68d290deb5526bbea81f1b0a7bca87dfb4f63
nithinK-142 <nithin.sagar.359@gmail.com>
2024-07-04 15:06:52 +0530

    add: static page.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import { getRepoLinks } from "../controller/repoLinks.controller";
+import path from "path";
 
 const router = Router();
 
-router.get("/", (_req, res) => res.send("Branch Sentinel"));
+router.use("/", express.static(path.join(__dirname, "../../public/out")));
+
 router.get("/api/v1", getRepoLinks);
 
 export { router as BranchRouter };
